4bbc83e08e1b2d83155c8e3bd1b9603c2daa3cd6
Patel-Pratham-3492 <patelpratham8320@gmail.com>
2025-11-05 13:50:27 -0600

    remove Landing page and start with login

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import Host from "./assets/components/host/host";
 import Manager from "./assets/components/Manager/manager";
 import Customerdash from './assets/components/customer/customerdash';
-import LandingPage from "./assets/components/landingPage/landingPage";
 import Login from "./assets/components/loginPage/Login";
 import Signup from "./assets/components/signupPage/signup";
 import Waiter from './assets/components/waiter/waiter';
@@ -19,8 +18,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/host" element={<Host />} />
         <Route path="/manager" element={<Manager />} />
